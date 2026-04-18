a8abb784a68c097dcb79625de46b107b559a50c5
xuhaihua <xuhaihua@xuhaihuadeMacBook-Pro.local>
2026-04-18 09:42:05 +0800

    complete k_shot_prompting

--- week1/k_shot_prompting.py
+++ week1/k_shot_prompting.py
@@ -7,7 +7,10 @@ load_dotenv()
 NUM_RUNS_TIMES = 5
 
 # TODO: Fill this in!
-YOUR_SYSTEM_PROMPT = ""
+YOUR_SYSTEM_PROMPT = """
+example4: input is status, output is sutats
+example5: input is http, output is ptth
+"""
 
 USER_PROMPT = """
 Reverse the order of letters in the following word. Only output the reversed word, no other text:
